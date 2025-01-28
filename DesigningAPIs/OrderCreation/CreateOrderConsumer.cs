97c7b48915b3ce3df86e30451b57f734e3ef1b07
Irina Scurtu <scurtuirina@gmail.com>
2025-01-28 14:50:05 +0000

    ordercreation end

--- DesigningAPIs/OrderCreation/CreateOrderConsumer.cs
+++ DesigningAPIs/OrderCreation/CreateOrderConsumer.cs
@@ -1,5 +1,9 @@
-﻿using MassTransit;
+﻿using AutoMapper;
+using Contracts.Events;
+using MassTransit;
+using Orders.Domain.Entities;
 using OrdersApi.Models;
+using OrdersApi.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +14,32 @@ namespace OrderCreation
 {
     public class CreateOrderConsumer : IConsumer<OrderModel>
     {
-        public Task Consume(ConsumeContext<OrderModel> context)
+        private readonly IOrderService orderService;
+        private readonly IMapper mapper;
+
+        public CreateOrderConsumer(IOrderService orderService, IMapper mapper)
+        {
+            this.orderService = orderService;
+            this.mapper = mapper;
+        }
+
+
+        public async Task Consume(ConsumeContext<OrderModel> context)
         {
             //save the product in the database
             // access domain
             //Repository
             // Mappings -> OrderModel to Order
-            throw new NotImplementedException();
+            Console.WriteLine($"I got a command to create an order:{context.Message}");
+            //mapping from Message to an order object
+            var orderToAdd = mapper.Map<Order>(context.Message);
+            
+            /// Implement the logic to create an order
+            var savedOrder = await orderService.AddOrderAsync(orderToAdd);
+            /// send a notification to Admin
+            ///  Update customer            
+
+            await Task.CompletedTask;
         }
     }
 }
--- DesigningAPIs/OrderCreation/Program.cs
+++ DesigningAPIs/OrderCreation/Program.cs
@@ -1,10 +1,17 @@
 using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OrdersApi.Data.Repositories;
+using OrdersApi.Data;
+using OrdersApi.Service;
+using OrdersApi.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using OrdersApi.Infrastructure.Mappings;
+using Microsoft.EntityFrameworkCore;
 
 namespace OrderCreation
 {
@@ -19,6 +26,11 @@ namespace OrderCreation
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
+                    services.AddDbContext<OrderContext>(options => options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
+
+                    services.AddScoped<IOrderRepository, OrderRepository>();
+                    services.AddScoped<IOrderService, OrderService>();
+                    services.AddAutoMapper(typeof(OrderProfileMapping));
                     services.AddMassTransit(x =>
                     {
                         x.SetKebabCaseEndpointNameFormatter();
@@ -29,6 +41,10 @@ namespace OrderCreation
 
                         x.UsingRabbitMq((context, cfg) =>
                         {
+                            cfg.ReceiveEndpoint("create-order-command", e =>
+                            {
+                               //e.ConfigureConsumer<CreateOrderConsumer>(context);
+                            });
                             cfg.ConfigureEndpoints(context);
                         });
                     });
